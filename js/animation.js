1b19cddf4ae9221bf53a467ab6d27d1e6003fb92
Jeff Chapman <jeffcchapman@gmail.com>
2015-01-16 17:52:34 -0700

    through prepending the tweet

---
+++ js/animation.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+	$('#tweet-controls').hide();
+
+	$('.tweet-compose').on('click', function(){
+		$(this).css('height','5em');
+		$('#tweet-controls').show();
+	});
+
+	$('.tweet-compose').keyup(function(){
+		var text140 = $('.tweet-compose').val()
+		var textLeft = 140 - text140.length;
+		$('#char-count').html(textLeft);
+		  if (textLeft <= 10) {
+		  	$('#char-count').css('color', 'red');
+		  }
+		  if (textLeft > 10) {
+		  	$('#char-count').css('color', 'black');
+		  }
+		  if (textLeft < 0) {
+		  	$('#tweet-submit').prop('disabled', true);
+		  }		
+		  if (textLeft >= 0) {
+		  	$('#tweet-submit').prop('disabled', false);
+		  }
+	});
+
+	$('#tweet-submit').on('click', function(){
+		var tweet = $('.tweet-compose').val();
+		var userName = $('#profile-summary p').text() + ' ';
+		var pic = $('#profile-summary .avatar').attr('src');
+		$('#stream').prepend(
+			'<div class="tweet"><div class="content"><img class="avatar" src="' + pic + '" /><strong class="fullname">' + userName + '</strong><span class="username">' + userName + '</span><p class="tweet-text">' + tweet + '</p><div class="tweet-actions"><ul><li><span class="icon action-reply"></span> Reply</li><li><span class="icon action-retweet"></span> Retweet</li><li><span class="icon action-favorite"></span> Favorite</li><li><span class="icon action-more"></span> More</li></ul></div>'
+			);
+	})
+
+//click into text box
+//every keystroke
+//subtract 1 from charcount
+
+
+
+
+
+})
